529fb6df218ac76a0a3d6b0110d616a73b7e1bd1
Xenax33 <saadkbr8@gmail.com>
2026-01-27 02:10:10 +0500

    added migration for lost data 2

--- prisma/check-invoice-exists.sql
+++ prisma/check-invoice-exists.sql
@@ -8,27 +8,4 @@ SELECT
   invoice_date,
   created_at
 FROM invoices 
-WHERE fbr_invoice_number = '3520223926179DIABBBOM380503' 
-   OR invoice_ref_no = 'INV-2026-01-09-0001';
-
--- Check invoice items for this FBR number
-SELECT 
-  ii.*
-FROM invoice_items ii
-JOIN invoices i ON ii.invoice_id = i.id
-WHERE i.fbr_invoice_number = '3520223926179DIABBBOM380503';
-
--- Check constraints on invoices table
-SELECT
-    con.conname AS constraint_name,
-    con.contype AS constraint_type,
-    CASE
-        WHEN con.contype = 'u' THEN 'UNIQUE'
-        WHEN con.contype = 'p' THEN 'PRIMARY KEY'
-        WHEN con.contype = 'f' THEN 'FOREIGN KEY'
-        WHEN con.contype = 'c' THEN 'CHECK'
-    END AS constraint_description,
-    pg_get_constraintdef(con.oid) AS constraint_definition
-FROM pg_constraint con
-JOIN pg_class rel ON rel.oid = con.conrelid
-WHERE rel.relname = 'invoices';
+WHERE fbr_invoice_number = '3520223926179DIABBBOM380503';
